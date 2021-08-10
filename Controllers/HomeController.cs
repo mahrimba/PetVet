247cee411d963263bc540632d0b9bd7e23d47387
mahrimba <matjuni@gmail.com>
2021-08-10 09:26:32 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PetVet.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteVeterinaria.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Servicos()
+        {
+            return View();
+        }
+
+        public IActionResult Agendamento()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Agendamento(Agendamento pet)
+        {
+            ViewBag.nome = pet.Nome;
+            ViewBag.telefone = pet.Telefone;
+            ViewBag.data = pet.Data;
+            ViewBag.animal = pet.Animal;
+            ViewBag.necessidade = pet.Necessidade;
+            return View("Detalhe");
+        }
+
+        public IActionResult Detalhe()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Agendamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetVet.Models
+{
+    public class Agendamento
+    {
+        public string Nome { get; set; }
+        public int Telefone { get; set; }
+
+        public string Data { get; set; }
+
+        public string Animal { get; set; }
+
+        public string Necessidade { get; set; }
+    }
+}
